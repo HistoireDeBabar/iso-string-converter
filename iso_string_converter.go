e053eecb5524cfebdd07905f56e89fa1b4633991
HistoireDeBabar <p.barber1990@gmail.com>
2016-01-10 09:04:43 +0000

    init

---
+++ iso_string_converter.go
@@ -0,0 +1,60 @@
+// Package provides a converter for iso time formatted strings
+// into a Time object.
+package iso_converter
+
+import (
+	"strings"
+	"time"
+)
+
+var (
+	months = map[string]string{
+		"01": "Jan",
+		"02": "Feb",
+		"03": "Mar",
+		"04": "Apr",
+		"05": "May",
+		"06": "Jun",
+		"07": "Jul",
+		"08": "Aug",
+		"09": "Sep",
+		"10": "Oct",
+		"11": "Nov",
+		"12": "Dec",
+	}
+)
+
+// given a ISO Time formatted string, returns a Time instance
+// Is string is invalid, returns a Zero Time.
+func IsoStringToDate(isoTimeValue string) (setTime time.Time) {
+	if isoTimeValue == "" {
+		return time.Time{}
+	}
+
+	if !strings.Contains(isoTimeValue, "-") {
+		return time.Time{}
+	}
+
+	timeSplit := strings.Split(isoTimeValue, "-")
+
+	if len(timeSplit) != 3 {
+		return time.Time{}
+	}
+
+	year := timeSplit[0]
+	month := months[timeSplit[1]]
+	day := timeSplit[2]
+
+	shortTimeArray := []string{
+		year, month, day,
+	}
+
+	shortTime := strings.Join(shortTimeArray, "-")
+	const form = "2006-Jan-02"
+	setTime, err := time.Parse(form, shortTime)
+
+	if err != nil {
+		return time.Time{}
+	}
+	return setTime
+}
---
+++ iso_string_converter_test.go
@@ -0,0 +1,41 @@
+package iso_converter
+
+import (
+	"testing"
+	"time"
+)
+
+func TestISOStringConverterEmptyStringShouldEqualZeroTime(t *testing.T) {
+	timeString := ""
+	result := IsoStringToDate(timeString)
+	if !(result.IsZero()) {
+		t.Error("Result should be default time")
+	}
+}
+
+func TestISOStringConverterInvalidStringShouldEqualZeroTime(t *testing.T) {
+	timeString := "fhsoudfhsf"
+	result := IsoStringToDate(timeString)
+	if !(result.IsZero()) {
+		t.Error("Result should be default time")
+	}
+}
+
+func TestISOStringConverter(t *testing.T) {
+	timeString := "2016-02-21"
+	result := IsoStringToDate(timeString)
+	if result.IsZero() {
+		t.Error("Result should be not be Zero time")
+	}
+	if result.Month() != time.February {
+		t.Errorf("result Month should be Febuary not %v", result.Month())
+	}
+
+	if result.Day() != 21 {
+		t.Errorf("result Day should be 21 not %v", result.Day())
+	}
+
+	if result.Year() != 2016 {
+		t.Errorf("result Day should be 2016 not %v", result.Year())
+	}
+}
